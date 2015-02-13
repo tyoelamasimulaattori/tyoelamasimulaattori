974426bfc5354fc07f47d08b49fbc266bb0f9731
Anni Janger <janger.anni.x@student.uta.fi>
2015-02-13 14:21:52 +0200

    tein muutoksia joopajoo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="fi">
 <head>
-	<title>Työelämäsimulaattori</title>
+	<title>Työelämäsimulaattori KOKEILU</title>
   <script type="text/javascript" src="js/main.js"></script>
   <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-	<h1>foofoo</h1>
+	<h1>foofoo foofoo</h1>
 </body>
 </html>
