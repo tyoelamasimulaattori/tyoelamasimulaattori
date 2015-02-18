7aed22406541af81020a3cab7d898f3a0b806f91
Riku Rouvila <riku.rouvila@gmail.com>
2015-02-18 18:57:19 +0200

    styles for modal and button, initial template for first view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,24 @@
   <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-	<h1>foofoo foofoo</h1>
-=======
-	<h1>Minä Muutin tätä!</h1>
+	<div class="modal">
+    <div class="modal__dialog">
+      <h1>Tervetuloa</h1>
+      <p>
+        Tämä on PSHP:n työelämäsimulaattori.
+        Simulaattorin tavoite on tarjota interaktiivista koulutusta esimiehille, hyödynteän realistisia työelämän ongelmia, joita ratkoa.
+        Simulaattori etenee seuraavalla tavalla:
+        <ul>
+          <li>Ensin syötä käyttäjätietosi ja valitse lähestymisnäkökulmasi annetuista vaihtoehdoista.</li>
+          <li>Simulaattori generoi sinulle kolme tapausta valitsemastasi näkökulmasta, joista voit yhden kokeiltavaksi</li>
+          <li>Tapauksessa sinulle esitetään tilanne ja siihen erilaisia ratkaisutapoja. Hyödytäen vinkkeja ja muuta materiaalia, valitse mielestäsi paras vaihtoehto</li>
+          <li>Valittuasi vaihtoehdon, tapaus etenee seuraavaan vaiheeseen. Jatka vaeihtoehtojen valitsemista kunnes tapaus pääsee loppuun.</li>
+          <li>Lopussa sinulle esitetään  sinun vastaukset ja ideaalisuoritus, jonka jälkeen voit joko lopettaa tai aloittaa uuden tapauksen</li>
+        </ul>
+      </p>
+      <p>Ennen kuin aloitat simulaattorin, varmista, että sinulla on oma DiSC - profiili, sillä tulet tarvitsemaan sitä simulaatiossa.</p>
+      <button class="btn">Aloita simulaatio</button>
+    </div>
+  </div>
 </body>
 </html>
