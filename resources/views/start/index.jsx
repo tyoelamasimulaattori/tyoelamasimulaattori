1344501944c8d32d1523fb3ff6740dfc9504bfac
MartinHeath <martin.heath@luotaajat.fi>
2015-02-22 16:23:24 +0200

    Index.jsx:                   lähestymistapa -> kyvykkyystekijä

--- resources/views/start/index.jsx
+++ resources/views/start/index.jsx
@@ -9,7 +9,7 @@ module.exports = React.createClass({
   render: function() {
     return (
       <View>
-        <h1>Valitse lähestymisnäkökulma</h1>
+        <h1>Valitse kyvykkyystekijä</h1>
         <RouteHandler/>
       </View>
     );
