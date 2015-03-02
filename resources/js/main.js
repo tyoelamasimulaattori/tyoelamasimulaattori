a4589bed9dd679bfcf89b8efd6504f0d6b0921a5
MartinHeath <martin.heath@luotaajat.fi>
2015-03-02 14:26:21 +0200

    Attempt at creating the final page:  * Created "end-screen" view  * Added placeholder text and button to main view  * Created small style sheet for end-screen  * Managed to create Route to end-screen  * Result was not satisfactory
    Will update and edit in the future.

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,10 +6,12 @@ import { default as Router, Route } from 'react-router';
 import { default as StartView } from 'views/start';
 import { default as DiscSelectView } from 'views/start/disc-select';
 import { default as IntroView } from 'views/start/intro';
+import { default as EndScreen } from 'views/start/end-screen';
 
 const routes = (
   <Route handler={App} path="/">
     <Route handler={StartView}>
+      <Route name= "end" path ="end" handler={EndScreen} />
       <Route name="disc" path="disc" handler={DiscSelectView} />
       <Route name="intro" path="intro" handler={IntroView} />
     </Route>
--- resources/views/start/index.jsx
+++ resources/views/start/index.jsx
@@ -1,6 +1,7 @@
 import { default as React } from 'react';
 import { RouteHandler, Navigation } from 'react-router';
 import { default as View } from 'components/view';
+import { default as Button } from 'components/button';
 
 export default React.createClass({
   mixins: [Navigation],
@@ -8,7 +9,15 @@ export default React.createClass({
     return (
       <View>
         <h1>Valitse kyvykkyystekijä</h1>
+        <div className = "kyvykkyys_text">
+          <p> Bacon ipsum dolor amet filet mignon sausage fatback shoulder frankfurter pork loin ribeye. Tenderloin prosciutto ribeye alcatra turducken pancetta leberkas. Picanha alcatra kevin beef pancetta swine frankfurter pastrami shankle ham hock doner.
+          Prosciutto pancetta capicola corned beef bresaola porchetta rump meatball spare ribs jowl ham short ribs sausage swine biltong.
+          </p>
+          <Button to="end">
+            Loppuun
+          </Button>
         <RouteHandler/>
+        </div>
       </View>
     );
   },
