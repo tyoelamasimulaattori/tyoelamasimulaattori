81f9d144191f2a632a298e979ef156d35637ebb2
Riku Rouvila <riku.rouvila@gmail.com>
2015-03-17 23:30:43 +0200

    add dynamic, inputted values to person card

--- resources/components/person-card/index.jsx
+++ resources/components/person-card/index.jsx
@@ -1,14 +1,20 @@
 import { default as React } from 'react';
 
 export default React.createClass({
+  propType: {
+    name: React.PropTypes.string.isRequired,
+    title: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string.isRequired,
+    image: React.PropTypes.string.isRequired
+  },
   render() {
     return (
       <div className="person-card">
         <div className="person-card__header">
-          <img className="person-card__thumbnail" src="/images/1.png" />
-          <h2>Matti Meikäläinen</h2>
-          <h3>Ohjelmistosuunnittelija</h3>
-          <p> Matti on rauhallinen ja ystävällinen. Työskentele usein aamusta iltaan jos voi. Ei pidä paahtoleivästä.</p>
+          <img className="person-card__thumbnail" src={this.props.image} />
+          <h2>{this.props.name}</h2>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.description}</p>
         </div>
       </div>
     )
