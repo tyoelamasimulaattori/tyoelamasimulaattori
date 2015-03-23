255d991b74052d913d1059a7ee6eec828fd1c753
Riku Rouvila <riku.rouvila@gmail.com>
2015-03-23 23:27:40 +0200

    fix wrong class name

--- app/Perspective.php
+++ app/Perspective.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Perspective extends Model {
   public function cases() {
-    return $this->hasMany('App\GameCase');
+    return $this->hasMany('App\Gamecase');
   }
 }
