c874da6d9611f6fb3882443695db7a3ea8e90a11
Riku Rouvila <riku.rouvila@gmail.com>
2015-03-23 19:26:32 +0200

    endview cleanup
    fixed indentation, removed unnecessary conditional modal creation (is already shown/hidden when the classname changes)
    
    https://github.com/tyoelamasimulaattori/tyoelamasimulaattori/commit/42b3d5b99d13affe88e7416014627db14a86f31a

--- resources/views/end/index.jsx
+++ resources/views/end/index.jsx
@@ -16,14 +16,12 @@ export default React.createClass({
     this.setState({
       isModalOpen: !this.state.isModalOpen
     });
-    console.log(this.state.isModalOpen);
   },
   render() {
+
     const route = this.props.currentCase.steps.map((step) => {
       return (
-        <p>
-          {step.description}
-        </p>
+        <p>{step.description}</p>
       )
     });
 
@@ -31,42 +29,41 @@ export default React.createClass({
       end: true
     });
 
-    var classNames = classSet({
+    const classNames = classSet({
       'modal': true,
       'modal--hidden': !this.state.isModalOpen,
     });
-    var modal = null;
 
-    if(this.state.isModalOpen) {
-        modal = (<div className={classNames}>
-          <div className="modal__dialog">
-            <h1>Demo on päättynyt</h1>
-            <p>
-              Tämä demo työelämäsimulaattorista loppuu tähän. Lopullisessa pelissä tulee olemaan
-              monta tapausta, joissa askeleita on paljon enemmän.
-            </p>
-
-            <p>
-              Kiitos kun kokeilit demoa! Paina alla olevaa nappian nähdäksesi valintojesi
-              lopputuloksen.
-            </p>
-            <Button onClick={this.handleToggle}>
-              Loppunäkymään
-            </Button>
-          </div>
-        </div>)
-    }
-    var backgroundStyle = {
+    const backgroundStyle = {
       backgroundImage: `url(${imagePath(currentStep.image)})`
     };
 
+    const modal = (
+      <div className={classNames}>
+        <div className="modal__dialog">
+          <h1>Demo on päättynyt</h1>
+          <p>
+            Tämä demo työelämäsimulaattorista loppuu tähän. Lopullisessa pelissä tulee olemaan
+            monta tapausta, joissa askeleita on paljon enemmän.
+          </p>
+
+          <p>
+            Kiitos kun kokeilit demoa! Paina alla olevaa nappian nähdäksesi valintojesi
+            lopputuloksen.
+          </p>
+          <Button onClick={this.handleToggle}>
+            Loppunäkymään
+          </Button>
+        </div>
+      </div>
+    )
+
     return (
       <View id="end-view">
-        {modal}
         <h1>Loppunäkymä</h1>
+
         <div className="row">
           <div className="column column1-5" style={backgroundStyle} />
-
           <div className="column column4-5">
             <p>
               {currentStep.description}
@@ -75,8 +72,8 @@ export default React.createClass({
         </div>
 
         <div className="row">
-		  <h3>Olet tullut demovaiheen loppuun, ja valitettavasti demovaiheessa ei voi jatkaa pidemmälle.</h3>
-		  <h3>Muistitko ottaa huomioon vastapuolen DiSC-profiilin?</h3>
+          <h3>Olet tullut demovaiheen loppuun, ja valitettavasti demovaiheessa ei voi jatkaa pidemmälle.</h3>
+          <h3>Muistitko ottaa huomioon vastapuolen DiSC-profiilin?</h3>
           <h3>Kokeile uutta tapausta eri näkökulmasta tai pohdi miten olisit voinut suoriuua vielä paremmin! </h3>
           <hr/>
           <h2> Valintasi pelin aikana: </h2>
@@ -87,6 +84,7 @@ export default React.createClass({
         <Button to="/">
           Lopeta
         </Button>
+        {modal}
       </View>
     );
   }
