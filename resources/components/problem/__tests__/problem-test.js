1ba2cffa98738440f072c0e4926101e894a6f2f8
MartinHeath <martin.heath@luotaajat.fi>
2015-03-30 12:53:28 +0300

    2nd attempt at test.

--- resources/components/problem/__tests__/problem-test.js
+++ resources/components/problem/__tests__/problem-test.js
@@ -14,28 +14,28 @@ describe('Problem', function() {
 
   it('should render a title, description, image and the number of questions, which are related to the given problem, in the inputted perspective', function() {
     var problem = {
-      {name: 'Problem 1'},
-      {description: 'Test problem #1 out of?'},
-      {image: '10.png'},
-      {answers: [
+      name: 'Problem 1',
+      description: 'Test problem #1 out of ?',
+      image: '10.png',
+      answers: [
         {name: 'Test question 1', correct: false},
         {name: 'Test question 2', correct: true},
         {name: 'Test question 4', correct: false}
-      ]}
+      ]
     };
     //stub onClick function
     var  onSelect = noop;
     // Render element into a document
     var problemTest = TestUtils.renderIntoDocument(
       <Problem
-        name={this.problem.name}
-        description={this.problem.description}
-        image={this.problem.image}
-        answers={this.problem.answers}
+        name={problem.name}
+        description={problem.description}
+        image={problem.image}
+        answers={problem.answers}
         onSelect={this.onSelect} />
     );
 
-    // Search all elements from rendered component with class name "case"
+    // Search all elements from rendered component with the given class name
     var questions = TestUtils.scryRenderedDOMComponentsWithClass(
       problemTest,
       'question'
@@ -50,7 +50,7 @@ describe('Problem', function() {
     );
 
     equal(problems.length, 1);
-    equal(description, this.problem.description);
+    equal(description, problem.description);
     equal(questions.length, 3);
   });
 });
