c0d121623dee99adb5e4b68f53b598171e332b28
Riku Rouvila <riku.rouvila@gmail.com>
2015-04-06 13:29:17 +0300

    receive arguments to test script
    npm test -- --watch should now start watching tests for TTD

--- test.bat
+++ test.bat
@@ -1,10 +1,10 @@
 :: 2>/dev/null || echo "
 ::::::::::::::::::::::::::: Windows
 @ECHO OFF
-set NODE_PATH=resources&& node_modules\.bin\mocha --compilers js:babel/register resources/components/**/__tests__/*.js
+set NODE_PATH=resources&& node_modules\.bin\mocha --compilers js:babel/register resources/components/**/__tests__/*.js %*
 exit /b %ERRORLEVEL%
 
 " >/dev/null
 ########################### Unix
 #!/bin/bash
-export NODE_PATH=resources && node_modules/.bin/mocha --compilers js:babel/register resources/components/**/__tests__/*.js
+export NODE_PATH=resources && node_modules/.bin/mocha --compilers js:babel/register resources/components/**/__tests__/*.js "$@"
