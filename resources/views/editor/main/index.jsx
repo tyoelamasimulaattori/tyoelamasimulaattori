b7565015ebb54b6e3e6c8aa353b20d28736e680d
Anni Janger <janger.anni.x@student.uta.fi>
2015-04-28 12:10:31 +0300

    takaisin-button to editor main view

--- resources/views/editor/main/index.jsx
+++ resources/views/editor/main/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Header } from 'components/view';
+import { Button } from 'components';
 
 export default React.createClass({
   render() {
     return (
-      <div>
+      <div className="editor-main">
         <Header>Tehtävä-editori</Header>
         <p>
           Lorem ipsum Non in veniam aliqua culpa veniam ex consectetur enim dolore consequat do qui Excepteur consequat nulla enim Excepteur do laborum sit laborum qui amet.
         </p>
+        <Button warning className="back" to="/">Takaisin alkunäkymään</Button>
       </div>
     );
   }
